ce1029e4f24459246d8375648920c7bf7030ec4d
KEVIN ALEXANDER MAXI JARA <kmaxi@pichincha.com>
2023-03-10 12:33:53 -0500

    Add request name mapping 'billboard'

--- src/main/java/com/example/bookin/controller/BillboardController.java
+++ src/main/java/com/example/bookin/controller/BillboardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/billboard")
 @AllArgsConstructor
 public class BillboardController {
     private BillboardService billboardService;
